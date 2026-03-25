ff17734821cbfa9505e0e86fd87ea97c4532320c
Mohammad2460 <peaceinmind2460@gmail.com>
2026-03-26 02:01:13 +0530

    fix : flutter android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
         release {
             signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
